eccf0e94ccb72c61c26f7b1970c895f80cd94034
“BIT <ivanaidraza@gmail.com>
2021-05-04 11:17:11 +0200

    add switch in progess

---
+++ switch.js
@@ -0,0 +1,79 @@
+/*Napisi program za dane u nedelji od 1 do 7.Var a=day;*/
+var a = 1;
+switch (a) {
+    case 1:
+        console.log("monday")
+        break
+    case 2:
+        console.log("tuesday")
+        break
+    case 3:
+        console.log("wednesday")
+        break
+    case 4:
+        console.log("thusday")
+        break
+    case 5:
+        console.log("friday")
+        break
+    case 6:
+        console.log("saturday")
+        break
+    case 7:
+        console.log("sunday")
+}
+/*napisi program za dane u nedelji input broj od 1-7.Input 1="monday", input 10="input must be number betweeen 1 and 7"*/
+var a = 10;
+switch (a) {
+    case 1:
+        console.log("monday")
+        break
+    case 2:
+        console.log("tuesday")
+        break
+    case 3:
+        console.log("wednesday")
+        break
+    case 4:
+        console.log("thusday")
+        break
+    case 5:
+        console.log("friday")
+        break
+    case 6:
+        console.log("saturday")
+        break
+    case 7:
+        console.log("sunday")
+        break
+    default:
+        console.log("input must be a number")
+
+}
+/*Program 7 dana input number da li je dan ili vikend. Ostali, case 2="its a weekday,case 6="its weekend", case 12="must be a number 1-7"*/
+var a = 2;
+switch (a) {
+    case 1:
+        console.log("monday")
+        break
+    case 2:
+        console.log("it is a weekday")
+        break
+    case 3:
+        console.log("thusday")
+        break
+    case 4:
+        console.log("wednasday")
+        break
+    case 5:
+        console.log("thusday")
+        break
+    case 6:
+        console.log(" it is a weekend")
+        break
+    case 7:
+        console.log("friday")
+        break
+    default:
+        console.log("must be number between 1-7")
+}
